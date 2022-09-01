92e187e170a4d18c07ae8ccf09a9fb0b0356b4b0
germancrc <german.cruz36@gmail.com>
2022-09-01 13:04:48 -0400

    add animacion

--- js/main.js
+++ js/main.js
@@ -20,22 +20,57 @@ document.onreadystatechange = function() {
     }
 };
 
-// function reveal() {
-//   var reveals = document.querySelectorAll(".reveal");
+// ANIMACION ELEMENTOS
 
-//   for (var i = 0; i < reveals.length; i++) {
-//     var windowHeight = window.innerHeight;
-//     var elementTop = reveals[i].getBoundingClientRect().top;
-//     var elementVisible = 100;
+const servTextCont = document.getElementById('servTextCont');
+const servic2 = document.getElementById('servic2');
+const ubica = document.getElementById('ubica');
+const comunicar = document.getElementById('comunicar');
+const servicio1 = document.getElementById('servicio1');
+const servicio2 = document.getElementById('servicio2');
+const servicio3 = document.getElementById('servicio3');
+const servicio4 = document.getElementById('servicio4');
+const servicio5 = document.getElementById('servicio5');
+const servicio6 = document.getElementById('servicio6');
+const servicio7 = document.getElementById('servicio7');
+const servicio8 = document.getElementById('servicio8');
+const servicio9 = document.getElementById('servicio9');
+const servicio10 = document.getElementById('servicio10');
+const servicio11 = document.getElementById('servicio11');
+const servicio12 = document.getElementById('servicio12');
 
-//     if (elementTop < windowHeight - elementVisible) {
-//       reveals[i].classList.add("active");
-//     } else {
-//       reveals[i].classList.remove("active");
-//     }
-//   }
-// }
 
-// window.addEventListener("scroll", reveal);
+const mostrarServicio = (tarjetas, observador) => {
+
+  tarjetas.forEach((tarjeta) => {
+    if(tarjeta.isIntersecting){
+      tarjeta.target.classList.add('visible')
+    }
+  });
+}
+
+const observador = new IntersectionObserver(mostrarServicio, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5
+});
+
+observador.observe(servTextCont);
+observador.observe(servic2);
+observador.observe(ubica);
+observador.observe(comunicar);
+observador.observe(servicio1);
+observador.observe(servicio2);
+observador.observe(servicio3);
+observador.observe(servicio4);
+observador.observe(servicio5);
+observador.observe(servicio6);
+observador.observe(servicio7);
+observador.observe(servicio8);
+observador.observe(servicio9);
+observador.observe(servicio10);
+observador.observe(servicio11);
+observador.observe(servicio12);
+
 
 
